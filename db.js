69b2621b7f016fdfe99063f5e3792165c872f74f
Tuhin114 <tuhinpoddar114@gmail.com>
2023-10-23 11:21:13 +0530

    Thunder Client Collection + Some Update

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-exports.port = 3000;
+exports.port = 5000;
 exports.mongoURL =
   "mongodb+srv://root:fMLe5gJU3dRjguL@cluster0.wngvc8l.mongodb.net/myNoteBook?retryWrites=true&w=majority";
 exports.port;
