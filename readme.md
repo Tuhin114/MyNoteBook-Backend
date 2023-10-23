ff7f1e28457a7092113ad0d15be49f2313261851
Tuhin114 <tuhinpoddar114@gmail.com>
2023-10-23 12:55:54 +0530

    Added bycrypt Hash and  JWT aythentication

---
+++ readme.md
@@ -0,0 +1,3 @@
+to start please use - "npm run dev"
+For password hashing install - "npm i bcryptjs"
+For JWT authentication token - search JWT nodejs and jwt io in google and install the package as "npm i jsonwebtoken" in terminal
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 const { body, validationResult } = require("express-validator");
+var jwt = require("jsonwebtoken");
+
+const JWT_SECRET = "Tuhinisagoodb$oy";
 
 // Create a User using: POST "/api/auth/createuser". No login required.
 router.post(
@@ -27,14 +31,24 @@ router.post(
         return res.status(400).send("A user with that email already exists");
       }
 
+      const salt = await bcrypt.genSalt(10);
+      secPass = await bcrypt.hash(req.body.password, salt);
+
       //Create new user
       user = await User.create({
         name: req.body.name, // Change req.body.username to req.body.name
-        password: req.body.password,
+        password: secPass,
         email: req.body.email,
       });
 
-      res.json(user);
+      const data = {
+        user: {
+          id: user._id,
+        },
+      };
+      const authtoken = jwt.sign(data, JWT_SECRET);
+      // res.json(user);
+      res.json({ authtoken });
     } catch (error) {
       console.log(error.message);
       res.status(500).send("Server Error");
