1cb6d5104c5f351c3d9aeabc704ee45ca896587a
Tuhin114 <tuhinpoddar114@gmail.com>
2023-10-30 13:00:50 +0530

    Updated for Login

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+var cors = require("cors");
 const { port, mongoURL } = require("./db.js");
 const mongoose = require("mongoose");
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
